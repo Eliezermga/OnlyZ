3b7c9fd8b9b16ae67445825b6977c0451048ebe1
ELIEZER MUNUNGA <127545012+Eliezermga@users.noreply.github.com>
2025-09-30 17:22:49 +0200

    teste

--- app.py
+++ app.py
@@ -706,11 +706,8 @@ L'équipe Onlyz
 
 if __name__ == '__main__':
     with app.app_context():
-        # S'assurer que toutes les tables sont créées
         db.create_all()
-        
-        # Optionnel : créer quelques données de test si nécessaire
         if not User.query.first():
             print("Création des tables...")
-    
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get('PORT', 10000))  # Render fournit le port dans PORT
+    socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)
